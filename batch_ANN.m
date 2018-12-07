e005179f831924b1b0e3aeed092e5b014454587a
khock1 <45620184+khock1@users.noreply.github.com>
2018-12-07 16:01:16 +1000

    Add files via upload

---
+++ batch_ANN.m
@@ -0,0 +1,64 @@
+% Author: Karlo Hock, University of Queensland.
+
+% Run an ensebmle of Artificial Neural Networks (ANNs) for detection of pests in habitat patches
+% Set up ANN
+% Train each ANN separately using the training set
+% Retain the ANN that performs the best 
+% Test the performance of the retained ANN on the new test dataset
+% Plot the confusion matrix to see how well the retained ANN is handling the new data
+
+clear;
+% load a dummy dataset; the parameters are based on exploration of coral reefs with crown-of-thorns starfish outbreaks
+% the dataset contains 8 'environmental' variables, which can be continuous, discrete or binary
+% the output is binary, 1 = presence 0 = absence
+load('annbatchtrain.mat')
+input = train_data';% desginate data part of the ANN analysis
+target = train_output';% designate output part of the ANN analysis
+trainFcn = 'trainscg'; % training function
+hiddenLayerSize = 20; % hidden layer
+net = patternnet(hiddenLayerSize, trainFcn);% set up ANN
+net.input.processFcns = {'removeconstantrows', 'mapminmax'};
+net.divideFcn = 'dividerand';  % Divide data randomly
+net.divideMode = 'sample';  % Divide up every sample
+net.divideParam.trainRatio = 70/100;% 70% of the training data will be used to train ANN
+net.divideParam.valRatio = 15/100;% 15% of the training data will be used to validate ANN
+net.divideParam.testRatio = 15/100;% 15% of the training data will be used to test ANN
+net.performFcn = 'crossentropy';  % Cross-Entropy
+net.trainParam.showWindow = false;% no visualisation
+
+%criteria for determining which the best performign ANN to retain
+rejectall = 0.5;% overall rejection rate, overall fraction of misclassified samples must be lower than this value
+rejecttp = 0.5;% we also need to have a true positive rate that is higher than this value...
+rejecttn = 0.3;% ...and a true negative rate that is higher than this value, which should generally be less than true postiive criterion because we do not want to miss too many infested patches ...
+rejectdr = 0.7;% ... and finally we also need to have an overall precision that is higher than this value to make sure that we are not missing too many patches with pests
+stored_net = net;% container for the best performing network
+
+%run an ensemble of 200 individually trained ANNs to get the best performing one;
+%since each ANN will be slightly different based on how traning dataset was
+%randomly partitioned etc., we want to run a bunch of them to get the one with the
+%best perfomance to test on the new data
+for ann = 1:200
+    [net,tr] = train(net,input,target);% train a network with the training data usign the ANN specification above
+    output = net(input);% get the output of the ANN as probabilties of classification
+    [misclass_samples, conf_mat] = confusion(train_output', output);% get the fraction of misclassified samples and a confusion matrix for the traning dataset
+
+    truepos = conf_mat(2, 2)/(sum(conf_mat(:, 2)));% calculate the true postive rate of this ANN
+    trueneg = conf_mat(1, 1)/(sum(conf_mat(:, 1)));% calculate the true negative rate of this ANN
+    detectrate = conf_mat(2, 2)/(sum(conf_mat(2, :)));% calculate precision of this ANN
+    if ((truepos >= rejecttp) + (trueneg >= rejecttn) + (detectrate >= rejectdr) + (misclass_samples <= rejectall)) == 4% iff all the criteria are satisified
+        % use the classification performance values from the new network as the new criteria for future ANN rejection
+        rejectall = misclass_samples;
+        rejecttp = truepos;
+        rejecttn = trueneg;
+        rejectdr = detectrate;
+        % store the new best performing ANN and its output
+        stored_net = net;
+        stored_output = output;
+    end
+end
+
+% now test the best peforming ANN on some new data that was not included in the traning set
+ANN_output = stored_net(new_data');% get the output using the stored ANN
+[Lc,Lcm] = confusion(new_output', ANN_output);% get the confusion matrix between ANN predictions and actual output
+plotconfusion(new_output', ANN_output)% plot confusion matrix for the new data, to see how many true negatives etc we have in the new dataset with the retained ANN
+
